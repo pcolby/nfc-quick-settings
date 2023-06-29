bdcfdf876fe48fd580c4452293191647c7bef514
Paul Colby <git@colby.id.au>
2023-06-29 21:56:46 +1000

    Implement the basic tile features
    Not working yet.

--- app/src/main/java/NfcTileService.kt
+++ app/src/main/java/NfcTileService.kt
@@ -1,3 +1,7 @@
+import android.content.Intent
+import android.nfc.NfcAdapter
+import android.provider.Settings
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 
 class NfcTileService: TileService() {
@@ -9,6 +13,10 @@ class NfcTileService: TileService() {
     // Called when your app can update your tile.
     override fun onStartListening() {
         super.onStartListening()
+        var adapter: NfcAdapter? = NfcAdapter.getDefaultAdapter(this)
+        qsTile.state = if (adapter == null) Tile.STATE_UNAVAILABLE else
+            if (adapter.isEnabled) Tile.STATE_ACTIVE else Tile.STATE_INACTIVE
+        qsTile.updateTile()
     }
 
     // Called when your app can no longer update your tile.
@@ -19,6 +27,7 @@ class NfcTileService: TileService() {
     // Called when the user taps on your tile in an active or inactive state.
     override fun onClick() {
         super.onClick()
+        startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
     }
     // Called when the user removes your tile.
     override fun onTileRemoved() {
