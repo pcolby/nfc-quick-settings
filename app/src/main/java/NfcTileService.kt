f2758ff1092ce11c5685a78cd2fe5a79fbc1c9c6
Paul Colby <git@colby.id.au>
2023-06-29 21:16:31 +1000

    Add initial Quick Settings tile

---
+++ app/src/main/java/NfcTileService.kt
@@ -0,0 +1,27 @@
+import android.service.quicksettings.TileService
+
+class NfcTileService: TileService() {
+
+    // Called when the user adds your tile.
+    override fun onTileAdded() {
+        super.onTileAdded()
+    }
+    // Called when your app can update your tile.
+    override fun onStartListening() {
+        super.onStartListening()
+    }
+
+    // Called when your app can no longer update your tile.
+    override fun onStopListening() {
+        super.onStopListening()
+    }
+
+    // Called when the user taps on your tile in an active or inactive state.
+    override fun onClick() {
+        super.onClick()
+    }
+    // Called when the user removes your tile.
+    override fun onTileRemoved() {
+        super.onTileRemoved()
+    }
+}
