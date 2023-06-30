c96320c4a7b2a3f71c1fa296b4cdb632710471e1
Paul Colby <git@colby.id.au>
2023-06-30 19:52:38 +1000

    Fix the (one and only) test

--- app/src/androidTest/java/au/id/colby/nfcquicksettings/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/au/id/colby/nfcquicksettings/ExampleInstrumentedTest.kt
@@ -22,6 +22,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.nfcquicksettingstile", appContext.packageName)
+        assertEquals("au.id.colby.nfcquicksettings", appContext.packageName)
     }
 }
