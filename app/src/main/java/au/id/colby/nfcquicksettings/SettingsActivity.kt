809985b9cd69412d3d94611da2ee28c1e850b499
Paul Colby <git@colby.id.au>
2024-11-02 13:46:54 +1100

    Add initial Settings activity (WIP)
    The actual settings are mostly just those generated by the Android
    Sutdio wizard, with a few experimental tweaks and additions. This
    is a very early WIP.

---
+++ app/src/main/java/au/id/colby/nfcquicksettings/SettingsActivity.kt
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2023-2024 Paul Colby <git@colby.id.au>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+package au.id.colby.nfcquicksettings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.settings_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        }
+    }
+}
