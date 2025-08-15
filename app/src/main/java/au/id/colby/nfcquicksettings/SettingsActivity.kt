7a030a0b566834d909f674af18160125a8ca469d
Paul Colby <git@colby.id.au>
2025-08-15 22:26:07 +1000

    Remove unused import

--- app/src/main/java/au/id/colby/nfcquicksettings/SettingsActivity.kt
+++ app/src/main/java/au/id/colby/nfcquicksettings/SettingsActivity.kt
@@ -5,7 +5,6 @@ package au.id.colby.nfcquicksettings
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
