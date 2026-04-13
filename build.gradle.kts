0ebf5eb819e480a995be407f8bb1be83e49adbd8
Paul Colby <git@colby.id.au>
2026-04-13 18:00:00 +1000

    Bump Android Gradle plugin to v9.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "9.1.0" apply false
-    id("com.android.library") version "9.1.0" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
     id("org.jetbrains.dokka") version "2.2.0" apply false
     id("org.sonarqube") version "7.2.2.6593"
 }
