fc3bf9f3717993d57e02de8ab9cc7ce3b5cc0d40
Paul Colby <git@colby.id.au>
2026-05-08 19:05:07 +1000

    Bump SonarQube to 7.3.0.8198

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "9.2.1" apply false
     id("com.android.library") version "9.2.1" apply false
     id("org.jetbrains.dokka") version "2.2.0" apply false
-    id("org.sonarqube") version "7.2.3.7755"
+    id("org.sonarqube") version "7.3.0.8198"
 }
 
 sonar {
