11df0346c8823baddca69249e588944b9cebd0f8
Paul Colby <git@colby.id.au>
2026-06-19 21:27:45 +1000

    Bump androidx.core:core-ktx to v1.19.0
    Which requires that we target Android 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,7 +87,7 @@ base {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.18.0")
+    implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.14.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
