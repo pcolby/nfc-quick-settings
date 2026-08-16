9dcc3745016ab1afa9244d656f8029a5b9120ad9
Paul Colby <git@colby.id.au>
2026-08-16 18:08:26 +1000

    Remove the unused androidx.preference dependency
    The library is in maintenance mode now, and looks like it wasn't needed
    when originially added in 721bcb0f3081b3c5e2ad78b552abc5df19adf934.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,7 +90,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.19.0")
     implementation("androidx.appcompat:appcompat:1.8.0")
     implementation("com.google.android.material:material:1.14.0")
-    implementation("androidx.preference:preference-ktx:1.2.1") /// \todo This library is in maintenance mode.
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
